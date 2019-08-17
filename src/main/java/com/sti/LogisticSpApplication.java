63ce63c9ce8bbbb70fe2a4b05aa07ef6df76db72
macbook <macbook@192.168.0.43>
2019-08-17 05:45:20 -0500

    Commit no.1 Configuración inicial del proyecto Logistic, solo se creo el protecto principal aun no se crean los paquetes ni clases, solo esta lo que viene por default

---
+++ src/main/java/com/sti/LogisticSpApplication.java
@@ -0,0 +1,13 @@
+package com.sti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticSpApplication.class, args);
+	}
+
+}
